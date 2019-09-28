61750788607c8cbc40a79a3a2c0f2ffc03f69026
thejhubbs <thejhubbs@gmail.com>
2019-09-28 16:02:16 -0400

    Connecting to prod pg

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' }
   }
