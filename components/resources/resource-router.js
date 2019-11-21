7a55a8a5a6fb774a194ba36e93bbe9279e8137f0
thejhubbs <thejhubbs@gmail.com>
2019-11-21 16:03:34 -0500

    Extended jsontoken length and added a get/id for resources

--- components/resources/resource-router.js
+++ components/resources/resource-router.js
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
 
   Resources.findById(id)
   .then(resource => {
-    res.status(201).json(resource);
+    res.status(200).json(resource);
   })
   .catch (err => {
     res.status(500).json({ message: 'Failed to find resource' });
