03f06e4045e160304473aee5baff06f3967982a5
thejhubbs <thejhubbs@gmail.com>
2019-12-12 21:24:48 -0500

    touch ups

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const server = express();
-const cors = require('cors');
-
-const coreRoutes = require('./core.js')
-
-server.use(cors());
-server.use(express.json());
 require('dotenv').config();
 
+const cors = require('cors');
+server.use(cors());
+
+server.use(express.json());
+
+//A lot functionality for the core of the webapp, including users, logs, feedback, pages, and blog posts.
+const coreRoutes = require('./core.js')
 server.use('/api', coreRoutes)
 
 server.get('/', (req, res) => {
