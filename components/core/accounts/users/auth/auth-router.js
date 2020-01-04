27e2ffd6501968034ddeeb77ac229be7cccf3a51
thejhubbs <thejhubbs@gmail.com>
2020-01-04 11:16:45 -0500

    Password bug

--- components/core/accounts/users/auth/auth-router.js
+++ components/core/accounts/users/auth/auth-router.js
@@ -390,7 +390,7 @@ async function setPassword(username, password, user_hash) {
   const hashCheck = checkForgotPasswordHash(user, user_hash)
 
   if (user && oneDayAfter && hashCheck) {
-    const updatedUser = await Users.updatePassword(hashedPassword, id)
+    const updatedUser = await Users.updatePassword(hashedPassword, user.user_id)
     return updatedUser
   } else {
     return false
